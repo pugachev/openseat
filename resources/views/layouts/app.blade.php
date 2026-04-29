0eafc193c7b31f05d3508dc10b18ae33209ce01a
pugachev <pugachev2011@gmail.com>
2026-04-29 18:12:51 +0900

    GUIの読み込み順の変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,6 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-    <script src="https://cdn.tailwindcss.com"></script>
-
     <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
