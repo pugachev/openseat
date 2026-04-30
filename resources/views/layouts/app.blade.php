dc3cfdeddbb5801e900818ef4b0d5e85c45c0afa
pugachev <pugachev2011@gmail.com>
2026-05-01 06:58:59 +0900

    ファビコンを設置

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Open Seat')</title>
+    <link rel="icon" type="image/svg+xml" href="{{ asset('map.svg') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
