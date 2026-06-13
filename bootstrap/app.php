84655a79d2fa49c4371560f310ba2bba71e717f3
pugachev <pugachev2011@gmail.com>
2026-06-13 12:56:30 +0900

    HTML レスポンスにキャッシュ禁止ヘッダーを付ける

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(append: [
             \App\Http\Middleware\TrackPageView::class,
+            \App\Http\Middleware\NoCacheHtml::class,
         ]);
 
         $middleware->alias([
