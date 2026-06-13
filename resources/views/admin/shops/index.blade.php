ee6b01387f9c78c4618c3081d6f54386249d6628
pugachev <pugachev2011@gmail.com>
2026-06-13 11:25:40 +0900

    レイアウト崩れ問題の解決 ユーザー→HTTPS→HTTP→Dockerになってた模様

--- resources/views/admin/shops/index.blade.php
+++ resources/views/admin/shops/index.blade.php
@@ -35,8 +35,8 @@
         </div>
     @else
         <div class="bg-white rounded-lg shadow-lg overflow-hidden">
-            <div class="table-responsive">
-            <table class="table min-w-full divide-y divide-gray-200">
+            <div class="overflow-x-auto">
+            <table class="min-w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                     <tr>
                         <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
